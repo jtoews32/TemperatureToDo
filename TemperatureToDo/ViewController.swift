f67eb96465f1fb7f69c40d01b91bb95dce6e3220
Jon Toews <jtoews@jons-imac.hsd1.ca.comcast.net>
2015-10-25 23:04:37 -0700

    Initial Commit

---
+++ TemperatureToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TemperatureToDo
+//
+//  Created by Jon Toews on 10/25/15.
+//  Copyright © 2015 Jon Toews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
